da1121a1d349c4e57537733caf2cba9a173e7ef8
Artem Kalinichenko <artem21kalinichenko@gmail.com>
2016-08-19 17:13:16 +0300

    21

--- app/js/common.js
+++ app/js/common.js
@@ -43,6 +43,36 @@ $(function() {
 		addClass: 'reviews',
 		pager:false
 	});
+	$('.slider-brands').lightSlider({
+		item:12,
+		loop:false,
+		slideMove:2,
+		easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+		speed:600,
+		prevHtml: "<",
+		nextHtml: ">",
+		pager: false,
+		responsive : [
+			{
+				breakpoint:800,
+				settings: {
+					item:3,
+					slideMove:1,
+					slideMargin:6,
+				}
+			},
+			{
+				breakpoint:480,
+				settings: {
+					item:2,
+					slideMove:1
+				}
+			}
+		],
+		onSliderLoad: function (el) {
+			el.next().appendTo(el.parents(".lSSlideOuter"));
+		}
+	});
 
 	$('.gamburger').on('click',function(){
 		$(this).toggleClass('active');
